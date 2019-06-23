b04cad1b75e5e231cc3b5891ae62763fd201e74a
Gonzalez Alayon <dgonzalezalayon@GAIG.COM>
2019-06-23 17:40:16 -0500

    added api-call-service

--- public/polyfills.js
+++ public/polyfills.js
@@ -5982,8 +5982,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\dgonzalezalayon\Desktop\forms\clientapp\src\polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! C:\Users\dgonzalezalayon\Desktop\forms\clientapp\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! C:\Users\dgonzalezalayon\Desktop\ng_express_forms\clientapp\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\Users\dgonzalezalayon\Desktop\ng_express_forms\clientapp\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
